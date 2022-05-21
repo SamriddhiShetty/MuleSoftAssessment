eb56c09e2b5d19d8432384a7c861b9a89a0c9686
Samriddhi Shetty <87483055+SamriddhiShetty@users.noreply.github.com>
2022-05-21 09:47:34 +0530

    Add files via upload

--- mulesoft_assessment.py
+++ mulesoft_assessment.py
@@ -1,23 +1,23 @@
 import sqlite3
-# conn=sqlite3.connect("movie_DB.db")
-# conn.execute(
-# '''CREATE TABLE MOVIES1
-#          (MOV_NAME VARCHAR2(50) PRIMARY KEY,
-#          ACTOR VARCHAR2(50),
-#          ACTRESS VARCHAR2(50),
-#          DIRECTOR VARCHAR2(20),
-#          YR_RELEASE VARCHAR2(10)
-# );
-# ''')
-# conn.close()
+conn=sqlite3.connect("movie_DB.db")
+conn.execute(
+'''CREATE TABLE MOVIES1
+         (MOV_NAME VARCHAR2(50) PRIMARY KEY,
+         ACTOR VARCHAR2(50),
+         ACTRESS VARCHAR2(50),
+         DIRECTOR VARCHAR2(20),
+         YR_RELEASE VARCHAR2(10)
+);
+''')
+conn.close()
 
 #INSERT STATEMENTS
-# conn=sqlite3.connect("movie_DB.db")
-# conn.execute("INSERT INTO MOVIES1 VALUES('DHOOM','AMIR','KATRINA','SANJAY','2018')")
-# conn.execute("INSERT INTO MOVIES1 VALUES('RRR','NTR','ALIA','RAJAMOULI','2022')")
-# conn.execute("INSERT INTO MOVIES1 VALUES('TARE ZAMEEN PAR','AMIR','MAYA','AMOLE','2007')")
-# conn.commit()
-# conn.close()
+conn=sqlite3.connect("movie_DB.db")
+conn.execute("INSERT INTO MOVIES1 VALUES('DHOOM','AMIR','KATRINA','SANJAY','2018')")
+conn.execute("INSERT INTO MOVIES1 VALUES('RRR','NTR','ALIA','RAJAMOULI','2022')")
+conn.execute("INSERT INTO MOVIES1 VALUES('TARE ZAMEEN PAR','AMIR','MAYA','AMOLE','2007')")
+conn.commit()
+conn.close()
 
 # #SELECT STATEMENTS
 
@@ -27,7 +27,6 @@ cur1.execute("SELECT * FROM MOVIES1 WHERE ACTOR='AMIR'")
 response1 = cur1.fetchall()
 print(response1)
 
-# conn=sqlite3.connect("movie_DB.db")
 cur2=conn.cursor()
 cur2.execute("SELECT * FROM MOVIES1 WHERE MOV_NAME='DHOOM'")
 response2 = cur2.fetchall()
