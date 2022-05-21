33ccf7d8d378ad8bff8e1b92eb01c730deaad66c
Samriddhi Shetty <87483055+SamriddhiShetty@users.noreply.github.com>
2022-05-21 09:46:10 +0530

    Add files via upload

--- mulesoft_assessment.py
+++ mulesoft_assessment.py
@@ -1,5 +1,5 @@
 import sqlite3
-# conn=sqlite3.connect("movies.db")
+# conn=sqlite3.connect("movie_DB.db")
 # conn.execute(
 # '''CREATE TABLE MOVIES1
 #          (MOV_NAME VARCHAR2(50) PRIMARY KEY,
@@ -9,26 +9,34 @@ import sqlite3
 #          YR_RELEASE VARCHAR2(10)
 # );
 # ''')
-# conn.close();
+# conn.close()
 
-# conn=sqlite3.connect("movies.db")
-# conn.execute("INSERT INTO MOVIES1 VALUES('DHOOM','AMIR','KATRINA','SANJAY','2018');")
-# conn.execute("INSERT INTO MOVIES1 VALUES('RRR','NTR','ALIA','RAJAMOULI','2022');")
-# conn.execute("INSERT INTO MOVIES1 VALUES('TARE ZAMEEN PAR','AMIR','MAYA','AMOLE','2007');")
+#INSERT STATEMENTS
+# conn=sqlite3.connect("movie_DB.db")
+# conn.execute("INSERT INTO MOVIES1 VALUES('DHOOM','AMIR','KATRINA','SANJAY','2018')")
+# conn.execute("INSERT INTO MOVIES1 VALUES('RRR','NTR','ALIA','RAJAMOULI','2022')")
+# conn.execute("INSERT INTO MOVIES1 VALUES('TARE ZAMEEN PAR','AMIR','MAYA','AMOLE','2007')")
 # conn.commit()
 # conn.close()
 
-conn=sqlite3.connect("movies.db")
-conn.execute("SELECT * FROM MOVIES1 WHERE ACTOR='AMIR';")
-response1=conn.cursor()
-details_mov=response1.fetchall()
-print(details_mov)
+# #SELECT STATEMENTS
 
-# conn=sqlite3.connect("movies.db")
-# conn.execute("SELECT * FROM MOVIES1 WHERE MOV_NAME='DHOOM';")
-# response2=conn.cursor()
-# details_act=response2.fetchall()
-# print(details_act)
+conn=sqlite3.connect("movie_DB.db")
+cur1=conn.cursor()
+cur1.execute("SELECT * FROM MOVIES1 WHERE ACTOR='AMIR'")
+response1 = cur1.fetchall()
+print(response1)
+
+# conn=sqlite3.connect("movie_DB.db")
+cur2=conn.cursor()
+cur2.execute("SELECT * FROM MOVIES1 WHERE MOV_NAME='DHOOM'")
+response2 = cur2.fetchall()
+print(response2)
+
+cur3=conn.cursor()
+cur3.execute("SELECT * FROM MOVIES1")
+response3 = cur3.fetchall()
+print(response3)
 
 conn.commit()
 conn.close()
